70ba5fce72364a0438c30a5c0d9e3bcf240a1a56
byronrds <byronrdsss761@gmail.com>
2025-08-28 08:01:35 -0400

    add prettier formatting

--- frontend/src/components/FavoriteCard.jsx
+++ frontend/src/components/FavoriteCard.jsx
@@ -4,61 +4,61 @@ import { useLocation, Link, Routes, Route, useParams } from 'react-router-dom';
 import '../styles/App.css';
 
 export const FavoriteCard = ({ calories, id, image, ingredientLines, source, label, uri, isEdamam }) => {
-	// For Edamam recipes, wrap in { recipe: ... } to match SingleRecipe expectations
-	const recipeData = isEdamam
-		? {
-			recipe: {
-				calories,
-				id,
-				image,
-				ingredientLines,
-				source,
-				label,
-				uri,
-				isEdamam,
-			},
-		}
-		: {
-			calories,
-			id,
-			image,
-			ingredientLines,
-			source,
-			label,
-			uri,
-			isEdamam,
-		};
+  // For Edamam recipes, wrap in { recipe: ... } to match SingleRecipe expectations
+  const recipeData = isEdamam
+    ? {
+        recipe: {
+          calories,
+          id,
+          image,
+          ingredientLines,
+          source,
+          label,
+          uri,
+          isEdamam,
+        },
+      }
+    : {
+        calories,
+        id,
+        image,
+        ingredientLines,
+        source,
+        label,
+        uri,
+        isEdamam,
+      };
 
-	return (
-		<div className='gimme-space'>
-			<Box maxWidth='800px'>
-				<Card>
-					<Flex gap='4' justify='between' align='start'>
-						<Box>
-							<Text as='div' size='4' weight='bold'>
-								{label}
-							</Text>
-							{ingredientLines.map((item, index) => (
-								<Text as='div' size='2' color='gray' key={index}>
-									- {item}
-								</Text>
-							))}
-							<Text as='div' size='2' weight='bold'>
-								Calories: {calories}
-							</Text>
-							<Text as='div' size='2' weight='bold'>
-								{source}
-							</Text>
-							<br></br>
-							<Link to={`/recipe/${id}`} state={{ recipe: recipeData }}>
-								<button>View</button>
-							</Link>
-						</Box>
+  return (
+    <div className="gimme-space">
+      <Box maxWidth="800px">
+        <Card>
+          <Flex gap="4" justify="between" align="start">
+            <Box>
+              <Text as="div" size="4" weight="bold">
+                {label}
+              </Text>
+              {ingredientLines.map((item, index) => (
+                <Text as="div" size="2" color="gray" key={index}>
+                  - {item}
+                </Text>
+              ))}
+              <Text as="div" size="2" weight="bold">
+                Calories: {calories}
+              </Text>
+              <Text as="div" size="2" weight="bold">
+                {source}
+              </Text>
+              <br></br>
+              <Link to={`/recipe/${id}`} state={{ recipe: recipeData }}>
+                <button>View</button>
+              </Link>
+            </Box>
 
-						<img style={{ width: '200px' }} src={image} fallback='T' />
-					</Flex>
-				</Card>
-			</Box>
-		</div>
-	);
+            <img style={{ width: '200px' }} src={image} fallback="T" />
+          </Flex>
+        </Card>
+      </Box>
+    </div>
+  );
 };
